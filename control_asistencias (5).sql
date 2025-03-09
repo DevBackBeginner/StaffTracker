63fe0347f7fb6c19ec32fee350c6fe59e76587ee
DevBackBeginner <morerahelbert9@gmail.com>
2025-03-08 21:54:55 -0500

    Actualizacion de la base de datos

---
+++ control_asistencias (5).sql
@@ -0,0 +1,452 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 09-03-2025 a las 03:54:18
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `control_asistencias`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `apoyo`
+--
+
+CREATE TABLE `apoyo` (
+  `usuario_id` int(11) NOT NULL,
+  `area_trabajo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `apoyo`
+--
+
+INSERT INTO `apoyo` (`usuario_id`, `area_trabajo`) VALUES
+(1, 'Atención al cliente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `asignaciones_computadores`
+--
+
+CREATE TABLE `asignaciones_computadores` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `computador_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `asignaciones_computadores`
+--
+
+INSERT INTO `asignaciones_computadores` (`id`, `usuario_id`, `computador_id`) VALUES
+(1, 1, 1),
+(4, 1, 4),
+(2, 2, 2),
+(3, 3, 3),
+(5, 5, 1),
+(6, 6, 2),
+(7, 7, 3),
+(8, 8, 4),
+(9, 9, 1),
+(10, 10, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `computadores`
+--
+
+CREATE TABLE `computadores` (
+  `id` int(11) NOT NULL,
+  `marca` varchar(50) NOT NULL,
+  `codigo` varchar(50) NOT NULL,
+  `tipo_computador` enum('Sena','Personal') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `computadores`
+--
+
+INSERT INTO `computadores` (`id`, `marca`, `codigo`, `tipo_computador`) VALUES
+(1, 'HP', 'HP001', 'Sena'),
+(2, 'Lenovo', 'LN002', 'Personal'),
+(3, 'Dell', 'DE003', 'Personal'),
+(4, 'Asus', 'AS004', 'Sena');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `directivos`
+--
+
+CREATE TABLE `directivos` (
+  `usuario_id` int(11) NOT NULL,
+  `cargo` varchar(100) NOT NULL,
+  `departamento` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `directivos`
+--
+
+INSERT INTO `directivos` (`usuario_id`, `cargo`, `departamento`) VALUES
+(9, 'Gerente', 'TI'),
+(10, 'Director', 'Finanzas');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `funcionarios`
+--
+
+CREATE TABLE `funcionarios` (
+  `usuario_id` int(11) NOT NULL,
+  `area` varchar(255) NOT NULL,
+  `puesto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`usuario_id`, `area`, `puesto`) VALUES
+(3, 'Finanzas', 'Contador'),
+(4, 'Recursos Humanos', 'Coordinador'),
+(6, 'Recursos Humanos', 'Atención al cliente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `guardas`
+--
+
+CREATE TABLE `guardas` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `turno` enum('mañana','tarde','noche') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `instructores`
+--
+
+CREATE TABLE `instructores` (
+  `usuario_id` int(11) NOT NULL,
+  `curso` varchar(255) NOT NULL,
+  `ubicacion` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `instructores`
+--
+
+INSERT INTO `instructores` (`usuario_id`, `curso`, `ubicacion`) VALUES
+(7, 'Programación en Python', 'Auditorio 1'),
+(8, 'Redes de Computadoras', 'Auditorio 2');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `registro_acceso`
+--
+
+CREATE TABLE `registro_acceso` (
+  `id` int(11) NOT NULL,
+  `asignacion_id` int(11) DEFAULT NULL,
+  `fecha` date NOT NULL DEFAULT curdate(),
+  `hora_entrada` time NOT NULL,
+  `hora_salida` time DEFAULT NULL,
+  `estado` enum('Activo','Finalizado') NOT NULL DEFAULT 'Activo',
+  `tipo_usuario` enum('personal_sena','visitante') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `registro_acceso`
+--
+
+INSERT INTO `registro_acceso` (`id`, `asignacion_id`, `fecha`, `hora_entrada`, `hora_salida`, `estado`, `tipo_usuario`) VALUES
+(32, 4, '2025-03-08', '13:00:43', '18:00:43', 'Activo', 'personal_sena');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `apellidos` varchar(260) NOT NULL,
+  `telefono` varchar(255) NOT NULL,
+  `numero_identidad` varchar(20) NOT NULL,
+  `rol` enum('Instructor','Funcionario','Directivo','Apoyo','Visitante','Admin','Guarda') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellidos`, `telefono`, `numero_identidad`, `rol`) VALUES
+(1, 'Helbert Dubler', 'Morera Hernández', '3105738706', '1072745267', 'Apoyo'),
+(2, 'Andres', 'Morera', '3106733829', '1072745268', 'Admin'),
+(3, 'Luis Pérez', 'Gómez García', '3101234567', '1072745269', 'Funcionario'),
+(4, 'Ana Martínez', 'Rodríguez López', '3107654321', '1072745270', 'Funcionario'),
+(5, 'Carlos Sánchez', 'Díaz Torres', '3109876543', '1072745271', 'Visitante'),
+(6, 'Marta López', 'Hernández Pérez', '3102468135', '1072745272', 'Instructor'),
+(7, 'Juan Castro', 'Pérez García', '3109871234', '1072745273', 'Funcionario'),
+(8, 'María Gómez', 'López Rodríguez', '3106543210', '1072745274', 'Funcionario'),
+(9, 'Pedro Jiménez', 'Molina Díaz', '3105738901', '1072745275', 'Directivo'),
+(10, 'Sara Ruiz', 'Fernández Torres', '3106733910', '1072745276', 'Directivo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios_autenticados`
+--
+
+CREATE TABLE `usuarios_autenticados` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `correo` varchar(100) NOT NULL,
+  `foto_perfil` varchar(255) DEFAULT NULL,
+  `contrasena` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios_autenticados`
+--
+
+INSERT INTO `usuarios_autenticados` (`id`, `usuario_id`, `correo`, `foto_perfil`, `contrasena`) VALUES
+(2, 2, 'luis@gmail.com', 'assets/img/perfiles/default.png', '$2y$10$g6nQKkqrb.IUY0nlpyKli.6XIhoq7y4BGPXkpl.5q8M4OyUCVgSJi');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `visitantes`
+--
+
+CREATE TABLE `visitantes` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) NOT NULL,
+  `asunto` varchar(260) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `visitantes`
+--
+
+INSERT INTO `visitantes` (`id`, `usuario_id`, `asunto`) VALUES
+(1, 5, 'Cita de matriculacion');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `apoyo`
+--
+ALTER TABLE `apoyo`
+  ADD PRIMARY KEY (`usuario_id`);
+
+--
+-- Indices de la tabla `asignaciones_computadores`
+--
+ALTER TABLE `asignaciones_computadores`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario_id` (`usuario_id`,`computador_id`),
+  ADD KEY `computador_id` (`computador_id`);
+
+--
+-- Indices de la tabla `computadores`
+--
+ALTER TABLE `computadores`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `codigo` (`codigo`);
+
+--
+-- Indices de la tabla `directivos`
+--
+ALTER TABLE `directivos`
+  ADD PRIMARY KEY (`usuario_id`);
+
+--
+-- Indices de la tabla `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD KEY `idx_funcionarios_area` (`area`),
+  ADD KEY `idx_funcionarios_puesto` (`puesto`);
+
+--
+-- Indices de la tabla `guardas`
+--
+ALTER TABLE `guardas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `instructores`
+--
+ALTER TABLE `instructores`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD KEY `idx_usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `registro_acceso`
+--
+ALTER TABLE `registro_acceso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `asignacion_id` (`asignacion_id`),
+  ADD KEY `idx_fecha_tipo_usuario` (`fecha`,`tipo_usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `numero_identidad` (`numero_identidad`),
+  ADD KEY `idx_numero_identidad` (`numero_identidad`);
+
+--
+-- Indices de la tabla `usuarios_autenticados`
+--
+ALTER TABLE `usuarios_autenticados`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- Indices de la tabla `visitantes`
+--
+ALTER TABLE `visitantes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `asignaciones_computadores`
+--
+ALTER TABLE `asignaciones_computadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `computadores`
+--
+ALTER TABLE `computadores`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `guardas`
+--
+ALTER TABLE `guardas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `registro_acceso`
+--
+ALTER TABLE `registro_acceso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios_autenticados`
+--
+ALTER TABLE `usuarios_autenticados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT de la tabla `visitantes`
+--
+ALTER TABLE `visitantes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `apoyo`
+--
+ALTER TABLE `apoyo`
+  ADD CONSTRAINT `apoyo_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `asignaciones_computadores`
+--
+ALTER TABLE `asignaciones_computadores`
+  ADD CONSTRAINT `asignaciones_computadores_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `asignaciones_computadores_ibfk_2` FOREIGN KEY (`computador_id`) REFERENCES `computadores` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `directivos`
+--
+ALTER TABLE `directivos`
+  ADD CONSTRAINT `directivos_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `funcionarios`
+--
+ALTER TABLE `funcionarios`
+  ADD CONSTRAINT `funcionarios_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `guardas`
+--
+ALTER TABLE `guardas`
+  ADD CONSTRAINT `guardas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `instructores`
+--
+ALTER TABLE `instructores`
+  ADD CONSTRAINT `instructores_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `registro_acceso`
+--
+ALTER TABLE `registro_acceso`
+  ADD CONSTRAINT `registro_acceso_ibfk_1` FOREIGN KEY (`asignacion_id`) REFERENCES `asignaciones_computadores` (`id`) ON DELETE SET NULL;
+
+--
+-- Filtros para la tabla `usuarios_autenticados`
+--
+ALTER TABLE `usuarios_autenticados`
+  ADD CONSTRAINT `usuarios_autenticados_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `visitantes`
+--
+ALTER TABLE `visitantes`
+  ADD CONSTRAINT `visitantes_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
