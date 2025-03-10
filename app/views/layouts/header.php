c54faea1a7efb2829e0c5e7032a5e98e11a22b91
JuanMa <juancho73x@gmail.com>
2025-03-10 15:12:42 -0500

    a

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -31,44 +31,52 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
    </head>
-   <!-- body -->
-   <body class="main-layout">
-      <!-- header -->
-      <header>
-         <!-- header inner -->
-         <div class="header">
-            <div class="container-fluid">
-               <div class="row">
-                  <div class="col-12 col-md-3 logo_section">
-                     <div class="full">
-                        <div class="center-desk">
-                           <div class="logo">
-                              <a href="Inicio"><img src="/ControlAsistencia/public/assets/img/logo.png" alt="#" class="img-fluid" /></a>
-                           </div>
+<!-- body -->
+<body class="main-layout">
+   <!-- header -->
+   <header>
+      <!-- header inner -->
+      <div class="header">
+         <div class="container-fluid">
+            <div class="row align-items-center">
+               <div class="col-12 col-md-3 logo_section">
+                  <div class="full">
+                     <div class="center-desk">
+                        <div class="logo">
+                           <a href="Inicio"><img src="/ControlAsistencia/public/assets/img/logo.png" alt="#" class="img-fluid" /></a>
                         </div>
                      </div>
                   </div>
-                  <div class="col-12 col-md-9">
-                     <nav class="navigation navbar navbar-expand-md navbar-dark">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarsExample04">
-                           <ul class="navbar-nav ml-auto">
-                              <li class="nav-item active">
+               </div>
+               <div class="col-12 col-md-9">
+                  <nav class="navbar navbar-expand-md navbar-light">
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation" style="border-color: #ffffff;">
+                     <span class="navbar-toggler-icon"></span>
+                    </button>
+                     <div class="collapse navbar-collapse" id="navbarMenu">
+                        <ul class="navbar-nav ms-auto">
+                           <li class="nav-item active">
+                              <div class="nav-link-container">
                                  <a class="nav-link" href="main.php">Inicio</a>
-                              </li>
-                              <li class="nav-item">
+                              </div>
+                           </li>
+                           <li class="nav-item">
+                              <div class="nav-link-container">
                                  <a class="nav-link" href="contacto.php">Contactos</a>
-                              </li>
-                              <li class="nav-item d-none d-md-block login_btn">
+                              </div>
+                           </li>
+                           <li class="nav-item login_btn">
+                              <div class="nav-link-container">
                                  <a class="nav-link" href="login">Login</a>
-                              </li>
-                           </ul>
-                        </div>
-                     </nav>
-                  </div>
+                              </div>
+                           </li>
+                        </ul>
+                     </div>
+                  </nav>
                </div>
             </div>
          </div>
-      </header>
+      </div>
+   </header>
+</body>
+</html>
